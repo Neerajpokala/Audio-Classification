449d300461e16be10c70aaa7bfc5b4a3c948a736
neerajpokala143 <73011282+neerajpokala143@users.noreply.github.com>
2024-02-23 23:03:45 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import requests
-from twilio.rest import Client
 
 API_URL = "https://api-inference.huggingface.co/models/MIT/ast-finetuned-audioset-10-10-0.4593"
 headers = {"Authorization": "Bearer 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"}
@@ -8,22 +7,6 @@ headers = {"Authorization": "Bearer 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"}
 def query(file_content):
     response = requests.post(API_URL, headers=headers, data=file_content)
     return response.json()
-
-def send_dnd_alert():
-    # Twilio API credentials
-    account_sid = 'AC423d9b4bac7fbac5ce51c21acd4d17c5'
-    auth_token = '[AuthToken]'
-    client = Client(account_sid, auth_token)
-
-    # Send DND Alert message
-    message = client.messages.create(
-        from_='+15304545628',
-        body='Screaming detected! Please do not disturb.',
-        to='+919059657973'
-    )
-
-    # Print the SID of the message
-    st.write("DND Alert message sent. SID:", message.sid)
 
 def about_project():
     st.title("About Project")
@@ -64,10 +47,6 @@ def voice_testing():
         st.write("Predicted labels:")
         if "Screaming" in labels:
             st.write("Screaming")
-            
-            # Send DND Alert using Twilio API
-            send_dnd_alert()
-            
         else:
             st.write(labels[0])
 
